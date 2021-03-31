eba8a81e98f49d5d85df15b9eec32ee2560bbfdc
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-31 09:38:50 -0300

    typeOf, numbers, booleans, functionsde conversão, parseInt, parseFloat

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ console.log(helloWorld);
 // console.log("Hello World!!!");
 
 
-// variáveis de escopo
+/* variáveis de escopo
+let é uma variável que muda, leva em consideração onde você declarou a variável, tem escopo de bloco
+const não pode mudar, são constantes. ex.: conexão do banco de dados ou algo desse tipo.
+*/
+
+// float ou ints são numbers em JavaScript
+
+// typeOf tipo de dado da variavél
+
+console.log(typeof(helloWorld));
+
+// instanceOf de qual instancia veio
+
+// boolean function linguagem fracamente tipada
+
+// functions de conversão parseInt, parseFloat
+
