70568c6aa9b68d7feb93ec956cd1f4fe81367594
pedrocarr <eng.pedro.amb@gmail.com>
2021-04-01 12:06:23 -0300

    functions

--- index.js
+++ index.js
@@ -99,3 +99,5 @@ for (let i = 0; i <= 10; i++) {
   console.log(`${i} X ${n} = ${i*n}`);
 
 }
+
+
---
+++ index1.js
@@ -0,0 +1,2 @@
+// funções são trechos de códigos que podem ser reutilizados
+// classe agrupa métodos e atributos no mesmo lugar
